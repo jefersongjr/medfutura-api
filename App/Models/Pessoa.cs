3f4d096bdf1e05c84964e318190e50942c1716be
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-08-31 19:22:10 -0300

    refactor :recycle: refatorando a model pessoa

--- App/Models/Pessoa.cs
+++ App/Models/Pessoa.cs
@@ -1,28 +1,24 @@
-namespace App.Models{
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-    using Microsoft.VisualBasic;
+namespace App.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Pessoa
-  {  
-    
-    [Key]
-    [Column("id")]
-    public int Id { get; set; }
+    {  
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
 
-    [Required]
-    [Column("apelido")]
-    public string Apelido { get; set; }
+        [Column("apelido")]
+        public string? Apelido { get; set; }
 
-    [Required]
-    [Column("nome")]
-    public string Nome { get; set; }
-   
-    [Column("nascimento")]
-    public DateOnly Nascimento { get; set; }
-    
-    [Column("stack")]
-    public string[] Stack { get; set; } = null!;
+        [Column("nome")]
+        public string? Nome { get; set; }
+       
+        [Column("nascimento")]
+        public DateOnly? Nascimento { get; set; }
+        
+        [Column("stack")]
+        public string[]? Stack { get; set; } = null!;
     }
 }
-
